2462dbdff669cb4777d35e0820b3d7ccc3ef2238
M Akbar <pxhxaxm@gmail.com>
2018-07-09 21:40:20 +0700

    un zip
    unzip file for easy editing

---
+++ Alone/Assets/Player_Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float movementSpeed = 3f;
+
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+		}
+	}
+}
