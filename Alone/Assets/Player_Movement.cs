0941907b4ca73f4da5d000a67f626b37d3931b03
M Akbar <pxhxaxm@gmail.com>
2018-07-09 23:52:56 +0700

    Animation and character
    update animation and player object prototipe

--- Alone/Assets/Player_Movement.cs
+++ Alone/Assets/Player_Movement.cs
@@ -5,12 +5,18 @@ public class Player_Movement : MonoBehaviour
 
     public float movementSpeed = 3f;
     public float jumpPower = 900;
-
+    public float turnSpeed = 10;
+    public GameObject PlayerObject;
+     Animator anim;
+    public static bool IsGrounded; 
     //local variable
     Rigidbody rb;
+    
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        anim = PlayerObject.GetComponent<Animator>();
+       
     }
     //fix update run smoot on low fps
     void FixedUpdate()
@@ -18,21 +24,45 @@ public class Player_Movement : MonoBehaviour
         //axis key can change on edit -> project seting -> input
         float horizontal = Input.GetAxis("Horizontal");
         Vector3 move = new Vector3(horizontal, 0, 0);
-        if (horizontal >= 0.1f || horizontal <= 0.1f)
+        if (horizontal >= 0.1f || horizontal <= -0.1f)
         {
             Movement(move);
+            if(IsGrounded)
+                anim.SetBool("move", true);
+            else anim.SetBool("move", false);
+        } else if (horizontal <= 0.1f && horizontal >= -0.1f)
+        {
+            anim.SetBool("move", false);
         }
 
         //jump [default : space key]
-        if (Input.GetButtonDown("Jump"))
+        if (IsGrounded)
         {
-            rb.AddForce(Vector3.up * jumpPower);
-            Debug.Log(Time.fixedDeltaTime);
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * jumpPower * Time.fixedDeltaTime);
+
+                Debug.Log(IsGrounded);
+            }
+            anim.SetBool("jump", false);
+        }
+        else {
+            anim.SetBool("jump", true);
         }
 
+        Debug.Log(IsGrounded);
     }
     void Movement(Vector3 _dir)
     {
         transform.Translate(_dir * movementSpeed * Time.fixedDeltaTime);
+        //rotate player
+        if (_dir.x <= 0)
+        {
+            PlayerObject.transform.rotation = Quaternion.Slerp(PlayerObject.transform.rotation, Quaternion.Euler(0, 270, 0),Time.fixedDeltaTime * turnSpeed);
+               
+        }
+        else {
+            PlayerObject.transform.rotation = Quaternion.Slerp(PlayerObject.transform.rotation, Quaternion.Euler(0, 90, 0), Time.fixedDeltaTime * turnSpeed);
+        }
     }
 }
