5f6405432ff1fde11ce77520e85e5a84a80cf404
M Akbar <pxhxaxm@gmail.com>
2018-07-09 22:43:51 +0700

    smoot movement
    smoot movement

--- Alone/Assets/Player_Movement.cs
+++ Alone/Assets/Player_Movement.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 
-public class Player_Movement : MonoBehaviour {
+public class Player_Movement : MonoBehaviour
+{
 
-	public float movementSpeed = 3f;
+    public float movementSpeed = 3f;
+    public float jumpPower = 900;
 
-	void Update () 
-	{
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-		}
-		else if(Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
-		}
-	}
+    //local variable
+    Rigidbody rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    //fix update run smoot on low fps
+    void FixedUpdate()
+    {
+        //axis key can change on edit -> project seting -> input
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(horizontal, 0, 0);
+        if (horizontal >= 0.1f || horizontal <= 0.1f)
+        {
+            Movement(move);
+        }
+
+        //jump [default : space key]
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpPower);
+            Debug.Log(Time.fixedDeltaTime);
+        }
+
+    }
+    void Movement(Vector3 _dir)
+    {
+        transform.Translate(_dir * movementSpeed * Time.fixedDeltaTime);
+    }
 }
